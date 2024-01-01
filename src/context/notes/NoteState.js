69b935c8cadcc30dc347b8459523810fa69b6dd3
mayankkumar <mayankkumar4x@gmail.com>
2024-01-01 14:22:15 +0530

    api url changed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NoteContext from "./noteContext";
 const NoteState=(props)=>{
-  const host = "http://localhost:5000";
+  const host = "https://nodejs-inotebook-api.onrender.com/";
     const notesInitial=[]
       const [notes,setNotes]=useState(notesInitial);
       //Get all Note
